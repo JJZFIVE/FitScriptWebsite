2f8b8596d3d6ccfaee132bcf92e24ff1045c2ed5
Joe Zakielarz <jjzfive@gmail.com>
2023-02-21 21:15:31 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,9 +16,16 @@ const nextConfig = {
         // Allow requests from your Vercel deployment
         source: "/",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
-            key: "Access-Control-Allow-Origin",
-            value: "https://fit-script-website.vercel.app",
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
