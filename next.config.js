d29644c0276f95b48baab9228f0dc22d99740fb9
Joe Zakielarz <jjzfive@gmail.com>
2023-02-21 21:10:08 -0500

    Update next.config.js for CORS on Vercel deployment

--- next.config.js
+++ next.config.js
@@ -9,6 +9,21 @@ const nextConfig = {
     API_URL: process.env.API_URL,
     WEBSITE_SIGNATURE: process.env.WEBSITE_SIGNATURE,
   },
+  // This allows CORS from Vercel deployment
+  async headers() {
+    return [
+      {
+        // Allow requests from your Vercel deployment
+        source: "/",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://fit-script-website.vercel.app",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
